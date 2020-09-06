bc1519b49f862421ce54042971aa699833aaad28
Ashish S <ashishjsharda@gmail.com>
2020-09-06 07:57:20 -0400

    Create vars2.kt

---
+++ vars2.kt
@@ -0,0 +1,12 @@
+object Vars {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val a:Int=100
+        val b:String="Sai"
+        val c:Double=200.00
+        println("Value of a is "+a)
+        println("Value of b is "+b)
+        println("Value of c is "+c)
+
+    }
+}
